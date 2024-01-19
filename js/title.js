6d7b8a85bef442af5f091339d73f780f97464684
twigicer <abin0926@qq.com>
2024-01-19 15:18:29 +0800

    Site updated: 2024-01-19 15:18:28

---
+++ js/title.js
@@ -0,0 +1,17 @@
+//动态标题
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        //离开当前页面时标签显示内容
+        document.title = '树叶落了🍃，你也走了';
+        clearTimeout(titleTime);
+    } else {
+        //返回当前页面时标签显示内容
+        document.title = '有你的照料，树苗茁壮正在成长🌱';
+        //两秒后变回正常标题
+        titleTime = setTimeout(function () {
+            document.title = OriginTitile;
+        }, 2000);
+    }
+});
