0822d763e63b20ade970db06a4341a8a39418445
twigicer <abin0926@qq.com>
2024-01-20 11:52:02 +0800

    Site updated: 2024-01-20 11:52:02

--- js/fps.js
+++ js/fps.js
@@ -25,17 +25,17 @@ if (window.localStorage.getItem("fpson") == undefined || window.localStorage.get
         if (now > 1000 + lastTime) {
             var fps = Math.round((frame * 1000) / (now - lastTime));
             if (fps <= 5) {
-                var kd = `<span style="color:#bd0000">卡成ppt🤢</span>`
+                var kd = `<span style="color:#bd0000">卡成ppt</span>`
             } else if (fps <= 15) {
-                var kd = `<span style="color:red">电竞级帧率😖</span>`
+                var kd = `<span style="color:red">电竞级帧率</span>`
             } else if (fps <= 25) {
-                var kd = `<span style="color:orange">有点难受😨</span>`
+                var kd = `<span style="color:orange">有点难受</span>`
             } else if (fps < 35) {
-                var kd = `<span style="color:#9338e6">不太流畅🙄</span>`
+                var kd = `<span style="color:#9338e6">不太流畅</span>`
             } else if (fps <= 45) {
-                var kd = `<span style="color:#08b7e4">还不错哦😁</span>`
+                var kd = `<span style="color:#08b7e4">还不错哦</span>`
             } else {
-                var kd = `<span style="color:#39c5bb">十分流畅🤣</span>`
+                var kd = `<span style="color:#39c5bb">十分流畅</span>`
             }
             document.getElementById("fps").innerHTML = `FPS:${fps} ${kd}`;
             frame = 0;
